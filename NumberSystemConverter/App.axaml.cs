e272f1fdd54493083933e2533980870cb13b9a00
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-09-25 19:58:56 +0200

    fix: remove AttachDeveleporTools, reason: suddenly caused error

--- NumberSystemConverter/App.axaml.cs
+++ NumberSystemConverter/App.axaml.cs
@@ -11,9 +11,6 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
-#if DEBUG
-        this.AttachDeveloperTools();
-#endif
     }
 
     public override void OnFrameworkInitializationCompleted()
